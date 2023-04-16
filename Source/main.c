1ebfb417a4fc287e298b7015fe03cf2365abf35a
Matiya DERUELLE <maderuel@e1r6p9.clusters.42paris.fr>
2023-04-16 17:30:45 +0200

    doing math

--- Source/main.c
+++ Source/main.c
@@ -6,7 +6,7 @@
 /*   By: maderuel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 13:47:43 by maderuel          #+#    #+#             */
-/*   Updated: 2023/04/04 15:02:12 by maderuel         ###   ########.fr       */
+/*   Updated: 2023/04/16 16:11:35 by maderuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <fract_ol.h>
@@ -38,12 +38,10 @@ int	main(void)
 	win->win = mlx_new_window(win->mlx, 500, 500, "win");
 	img->img = mlx_new_image(win->mlx, 300, 300);
 	img->addr = mlx_get_data_addr(img->img, &img->bpp, &img->ll, &img->end);
-	while (i++ < 100)
-		my_mlx_pixel_put(img, 50, i, 0x00FF0000);
+	while (i++ < 300)
+		my_mlx_pixel_put(img, 310, i, 0x00FF0000);
 	mlx_put_image_to_window(win->mlx, win->win, img->img, 0, 0);
-
 	mlx_hook(win->win, 2, 1L<<0, close, &win);
-
 	mlx_loop(win->mlx);
 	free(win);
 	free(img);
